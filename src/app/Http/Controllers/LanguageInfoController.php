8beb084f27f7387db30a2aa451e231a5382ee678
Philip Perry <philip.perry@learnclick.com>
2023-05-22 08:25:02 +0100

    excluding saving of empty settings

--- src/app/Http/Controllers/LanguageInfoController.php
+++ src/app/Http/Controllers/LanguageInfoController.php
@@ -106,10 +106,12 @@ class LanguageInfoController extends Controller
         if(isset($request->settings)) {
             $key = 0;
             foreach($request->settings as $key => $setting) {
-                $settings[$key]['languagepackid'] = $languagePack->id;
-                $settings[$key]['name'] = $key;
-                $settings[$key]['value'] = $setting;
-                $key++;
+                if(!empty($setting)) {
+                    $settings[$key]['languagepackid'] = $languagePack->id;
+                    $settings[$key]['name'] = $key;
+                    $settings[$key]['value'] = $setting;
+                    $key++;
+                }
             }    
             
             LanguageSetting::insert($settings);
