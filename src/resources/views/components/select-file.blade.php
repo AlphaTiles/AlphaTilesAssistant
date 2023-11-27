b02f5c30d61d5c7212e1ae785b89b5ee7e3b608a
Philip Perry <philip.perry@learnclick.com>
2023-11-27 19:42:00 +0000

    changed to use audio control

--- src/resources/views/components/select-file.blade.php
+++ src/resources/views/components/select-file.blade.php
@@ -14,19 +14,25 @@ if(empty($tile->{$typeField}) && $nr > 1) {
 }
 ?>
 
-<div id="show_file{{ $key }}_{{ $nr }}" class="{{$hideFileSelectionClass}}">
+<div id="show_file{{ $key }}_{{ $nr }}" class="{{$hideFileSelectionClass}} flex">
+    <div>
     {{ $nr }}:
     <input type="file" name="tiles[{{ $key }}][{{ $fileField }}]" class="custom-file-input" id="chooseFile" value="{{ $previousFileValue }}">
+    </div>    
+    <div>
     @if(isset($tile->{$fileRelation}) || isset($tile->{$filenameKey}))
         <?php 	        
         $filename = $tile->{$fileRelation}->name ?? $tile->{$filenameKey};
         $storedFileNumber = str_pad($tile->id, 3, '0', STR_PAD_LEFT); 
         ?>
-        <a href="/languagepack/tiles/{{ $tile->languagepackid }}/download/tile_{{ $storedFileNumber }}_{{ $nr }}.mp3">
-            {{ mb_strlen($filename) > 30 ? mb_substr($filename, 0, 30) . '...' : $filename }}
-        </a>
+        <audio controls style="width: 200px;">
+            <source src="/languagepack/tiles/{{ $tile->languagepackid }}/download/tile_{{ $storedFileNumber }}_{{ $nr }}.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio> 								
+
         <input type="hidden" name="tiles[{{ $key }}][{{ $filenameKey }}]" value="{{ $filename }}">
     @endif
+    </div>
     @if($errors->has('tiles.' . $key . '.' . $fileField))
         <div class="error">The file upload failed.</div>
     @endif									
